dcbb43bc499b8f718e839cfbb606c372144265cc
shahed7010 <shahed291prof@gmail.com>
2020-08-16 23:30:39 +0600

    wearing ppe fighting corona

---
+++ execution.js
@@ -0,0 +1,19 @@
+ 	
+	function wearPPE() {
+            return new Promise((resolve) => {
+                setTimeout(() => resolve('Wearing PPE'), 2000);
+            })
+        }
+        
+        function fightCorona() {
+            return new Promise((resolve) => {
+                setTimeout(() => resolve('Fighting Corona'), 1000);
+            })
+        }
+        
+        async function execute() {
+            await wearPPE().then(result => console.log(result));
+            fightCorona().then(result => console.log(result));
+        }
+        
+        execute();
